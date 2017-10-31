928ce765a67493fcd0ca16805b3f488937fc7f9a
mushu <showndarya.m@somaiya.edu>
2017-10-31 17:05:42 +0530

    Add SCAN.java

---
+++ Disk Scheduling/SCAN.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+class SCAN
+{
+	public static void main(String []args)
+	{
+		int ini_head,n,prev_head,trail_head;
+		
+		System.out.println("Enter initial header location: ");
+		Scanner sc = new Scanner(System.in);
+		ini_head = sc.nextInt();
+		
+		System.out.println("Enter previous header location: ");
+		prev_head = sc.nextInt();
+		
+		System.out.println("Enter trail track location: ");
+		trail_head = sc.nextInt();
+		
+		System.out.println("Enter number of disk locations: ");
+		n = sc.nextInt();
+		
+		int[] disk_location = new int[n];
+		int dl=0,seek_time=0,i,j,f=0;
+		
+		System.out.println("Enter disk locations: ");
+		for(i=0;i<n;i++) disk_location[i] = sc.nextInt();
+		
+		//Arrays.sort(disk_location);
+		
+		int[] visited = new int[n+1];
+		
+		System.out.println("Disk Location \t Seek Time");
+		
+		if(prev_head<=ini_head) f=0;
+		else f=1;
+		
+		for(i=0;i<n;i++)
+		{
+			int pos = -1;
+			int min = 10000;
+			for(j=0;j<n;j++)
+			{
+				//System.out.println(disk_location[j]+"/"+ini_head);
+				if(f==0)
+				{
+					//System.out.println(Math.abs(disk_location[j] - ini_head)+"//");
+					if(disk_location[j]>ini_head && min>Math.abs(disk_location[j]-ini_head) && visited[j]==0)
+					{
+						//System.out.println(Math.abs(disk_location[j] - ini_head));
+						min = Math.abs(disk_location[j] - ini_head);
+						pos = j;
+					}
+				}
+				
+				else if(f==1)
+				{
+					if(disk_location[j]<=ini_head && min>Math.abs(disk_location[j]-ini_head) && visited[j]==0)
+					{
+						//System.out.println(Math.abs(disk_location[j] - ini_head));
+						pos = j;
+						min = Math.abs(disk_location[j]-ini_head);
+					}
+				}
+			}
+			//System.out.println(pos);
+			if(pos==-1)
+			{
+				if(f==0)
+				{
+					f=1;
+					seek_time += Math.abs(trail_head - ini_head);
+					System.out.println(trail_head+"\t\t "+seek_time);
+					ini_head = trail_head;
+				}
+				else 
+				{
+					f=0;
+					seek_time += Math.abs(0 - ini_head);
+					System.out.println(0+"\t\t "+seek_time);
+					ini_head = 0;
+				}
+				System.out.println("-------------------------------------------------");
+				System.out.println("Changing Directions");
+				System.out.println("-------------------------------------------------");
+				System.out.println("Disk Location \t Seek Time");
+				i--;
+				continue;
+			}
+			visited[pos] = 1;
+			seek_time += Math.abs(disk_location[pos] - ini_head);
+			System.out.println(disk_location[pos]+"\t\t "+seek_time);
+			ini_head = disk_location[pos];
+		}
+		
+		System.out.println();
+		System.out.println("Total Seek Time :"+seek_time);
+	}
+}
